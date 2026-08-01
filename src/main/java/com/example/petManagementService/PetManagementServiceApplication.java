241dfd0b759f6af0a44d32b43c24bf48de432f1b
suyash jaiswal <suyash.jaiswal@antrepriz.com>
2026-08-02 00:19:00 +0530

    first comit

---
+++ src/main/java/com/example/petManagementService/PetManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.petManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/petManagementService/PetManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
